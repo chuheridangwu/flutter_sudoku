36240c2d538d7f18e6698d8ae474f6636d4470f5
dym <153100950@qq.com>
2019-10-08 21:23:00 +0800

    添加玩的时间

--- lib/game/game_view.dart
+++ lib/game/game_view.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_sudoku/mode/mode.dart';
@@ -24,6 +26,7 @@ class _GameViewPageState extends State<GameViewPage> {
                 children: <Widget>[
                   GameListView(),
                   LoveView(),
+                  TimeView(),
                   OperatingView(),
                   ItemView(),
                 ],
@@ -43,24 +46,23 @@ class GameListView extends StatelessWidget {
       showDialog(
           context: context,
           builder: (context) {
-            return  CupertinoAlertDialog(
+            return CupertinoAlertDialog(
               title: Text('数据重复'),
               actions: <Widget>[
                 FlatButton(
                   child: Text('取消'),
-                  onPressed: (){
+                  onPressed: () {
                     Navigator.of(context).pop('点击了取消');
                   },
                 ),
                 FlatButton(
                   child: Text('确定'),
-                  onPressed: (){
+                  onPressed: () {
                     Navigator.of(context).pop('点击了确定');
                   },
                 )
               ],
             );
-          
           });
     };
     return GridView.count(
@@ -135,12 +137,17 @@ class OperatingView extends StatelessWidget {
             onPressed: () {
               model.withdrawItem();
             },
+            color: Colors.red,
             child: Text('撤回'),
+          ),
+          SizedBox(
+            width: 10.0,
           ),
           FlatButton(
             onPressed: () {
               model.withdrawalItem();
             },
+            color: Colors.red,
             child: Text('回撤'),
           ),
         ],
@@ -197,29 +204,90 @@ class ItemView extends StatelessWidget {
   }
 }
 
-
 class LoveView extends StatefulWidget {
   @override
   _State createState() => _State();
 }
 
 class _State extends State<LoveView> {
-    List _loves = ["", "", ""]; // 爱心
+  List _loves = ["", "", ""]; // 爱心
 
-    void removeLastLove(){
-      setState(() {
-        _loves.removeLast();
-      });
-    }
+  void removeLastLove() {
+    setState(() {
+      _loves.removeLast();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: _loves.map((item){
-        return IconButton(icon: Icon(Icons.live_tv),onPressed: (){},);
-      }).toList(),
+    GameModel model = GameModel.of(context);
+    model.snpTipBack = () {
+      removeLastLove();
+    };
+    return Positioned(
+      left: 0,
+      top: MediaQuery.of(context).size.width,
+      right: 0,
+      child: Row(
+        children: _loves.map((item) {
+          return IconButton(
+            icon: Icon(Icons.live_tv),
+            onPressed: () {},
+          );
+        }).toList(),
+      ),
     );
   }
 }
 
+class TimeView extends StatefulWidget {
+  @override
+  _TimeViewState createState() => _TimeViewState();
+}
 
+class _TimeViewState extends State<TimeView> {
+  int _seconds = 0;
+  Timer _timer;
+  @override
+  void initState() {
+    super.initState();
+    const period = const Duration(seconds: 1);
+    _timer = Timer.periodic(period, (time) {
+      setState(() {
+        _seconds++;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    _timer = null;
+  }
+
+  //时间格式化，根据总秒数转换为对应的 hh:mm:ss 格式
+  String constructTime() {
+    int hour = _seconds ~/ 3600;
+    int minute = _seconds % 3600 ~/ 60;
+    int secode = _seconds % 60;
+    return formatTime(hour) +
+        ':' +
+        formatTime(minute) +
+        ':' +
+        formatTime(secode);
+  }
+
+  // 数字格式化 0~9
+  String formatTime(int timeNum) {
+    return timeNum < 10 ? '0' + timeNum.toString() : timeNum.toString();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      right: 10.0,
+      top: MediaQuery.of(context).size.width + 20,
+      child: Text('${constructTime()}'),
+    );
+  }
+}
